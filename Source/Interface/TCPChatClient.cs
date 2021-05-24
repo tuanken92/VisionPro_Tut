cfcf2bdb6f9557d071925de105b9cc58c06986cf
Anh Tuan <tuanken92@gmail.com>
2021-05-24 23:10:56 +0700

    [ADD] socket interface

---
+++ Source/Interface/TCPChatClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VisionPro_Tut.Source.Interface
+{
+    class ChatClient : TcpClient
+    {
+        public ChatClient(string address, int port) : base(address, port) { }
+
+        public void DisconnectAndStop()
+        {
+            _stop = true;
+            DisconnectAsync();
+            while (IsConnected)
+                Thread.Yield();
+        }
+
+        protected override void OnConnected()
+        {
+            Console.WriteLine($"Chat TCP client connected a new session with Id {Id}");
+        }
+
+        protected override void OnDisconnected()
+        {
+            Console.WriteLine($"Chat TCP client disconnected a session with Id {Id}");
+
+            // Wait for a while...
+            Thread.Sleep(1000);
+
+            // Try to connect again
+            if (!_stop)
+                ConnectAsync();
+        }
+
+        protected override void OnReceived(byte[] buffer, long offset, long size)
+        {
+            Console.WriteLine(Encoding.UTF8.GetString(buffer, (int)offset, (int)size));
+        }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat TCP client caught an error with code {error}");
+        }
+
+        private bool _stop;
+    }
+
+    class TcpChatClient
+    {
+
+    }
+
+}
---
+++ Source/Interface/TcpChatServer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisionPro_Tut.Source.Interface
+{
+    class ChatSession : TcpSession
+    {
+        public ChatSession(TcpServer server) : base(server) { }
+
+        protected override void OnConnected()
+        {
+            Console.WriteLine($"Chat TCP session with Id {Id} connected!");
+
+            // Send invite message
+            string message = "Hello from TCP chat! Please send a message or '!' to disconnect the client!";
+            SendAsync(message);
+        }
+
+        protected override void OnDisconnected()
+        {
+            Console.WriteLine($"Chat TCP session with Id {Id} disconnected!");
+        }
+
+        protected override void OnReceived(byte[] buffer, long offset, long size)
+        {
+            string message = Encoding.UTF8.GetString(buffer, (int)offset, (int)size);
+            Console.WriteLine("Incoming: " + message);
+
+            // Multicast message to all connected sessions
+            Server.Multicast(message);
+
+            // If the buffer starts with '!' the disconnect the current session
+            if (message == "!")
+                Disconnect();
+        }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat TCP session caught an error with code {error}");
+        }
+    }
+
+    class ChatServer : TcpServer
+    {
+        public ChatServer(IPAddress address, int port) : base(address, port) { }
+
+        protected override TcpSession CreateSession() { return new ChatSession(this); }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat TCP server caught an error with code {error}");
+        }
+    }
+
+    class TcpChatServer
+    {
+
+    }
+}
